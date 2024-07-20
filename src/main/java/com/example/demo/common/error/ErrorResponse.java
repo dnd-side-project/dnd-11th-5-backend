38fc1d37246c336f208494f34da1b8f573253aa0
dar-jeeling <o_o___t@pusan.ac.kr>
2024-07-20 17:47:44 +0900

    env: 프로젝트 초기 셋팅

---
+++ src/main/java/com/example/demo/common/error/ErrorResponse.java
@@ -0,0 +1,38 @@
+package com.example.demo.common.error;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ErrorResponse {
+
+	private int status;
+	private String code;
+	private String message;
+
+	@Builder
+	private ErrorResponse(int status, String code, String message) {
+		this.status = status;
+		this.code = code;
+		this.message = message;
+	}
+
+	public static ErrorResponse of(ErrorCode errorCode) {
+		return ErrorResponse.builder()
+			.status(errorCode.getStatus())
+			.code(errorCode.getCode())
+			.message(errorCode.getMessage())
+			.build();
+	}
+
+	public static ErrorResponse of(int status, String code, String message) {
+		return ErrorResponse.builder()
+			.status(status)
+			.code(code)
+			.message(message)
+			.build();
+	}
+}
