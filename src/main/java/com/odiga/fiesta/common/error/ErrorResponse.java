0ca5e29584554d797b180f7be9976ebcce73c0d3
dar-jeeling <o_o___t@pusan.ac.kr>
2024-07-20 21:21:39 +0900

    refactor: `ApiResponse`,`ErrorResponse` 의 상태코드 필드명 변경

--- src/main/java/com/odiga/fiesta/common/error/ErrorResponse.java
+++ src/main/java/com/odiga/fiesta/common/error/ErrorResponse.java
@@ -9,28 +9,28 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ErrorResponse {
 
-	private int status;
-	private String code;
+	private int statusCode; // http 상태코드
+	private String code; // 커스텀 에러 코드
 	private String message;
 
 	@Builder
-	private ErrorResponse(int status, String code, String message) {
-		this.status = status;
+	private ErrorResponse(int statusCode, String code, String message) {
+		this.statusCode = statusCode;
 		this.code = code;
 		this.message = message;
 	}
 
 	public static ErrorResponse of(ErrorCode errorCode) {
 		return ErrorResponse.builder()
-			.status(errorCode.getStatus())
+			.statusCode(errorCode.getStatus())
 			.code(errorCode.getCode())
 			.message(errorCode.getMessage())
 			.build();
 	}
 
-	public static ErrorResponse of(int status, String code, String message) {
+	public static ErrorResponse of(int statusCode, String code, String message) {
 		return ErrorResponse.builder()
-			.status(status)
+			.statusCode(statusCode)
 			.code(code)
 			.message(message)
 			.build();
