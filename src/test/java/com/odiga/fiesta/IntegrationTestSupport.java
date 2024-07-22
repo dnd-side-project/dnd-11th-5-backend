2c8872ed06d1611e9b67b6fe0fa01efd87651ec5
dar-jeeling <o_o___t@pusan.ac.kr>
2024-07-23 03:54:50 +0900

    feat: `IntegrationTestSupport` 구현

---
+++ src/test/java/com/odiga/fiesta/IntegrationTestSupport.java
@@ -0,0 +1,9 @@
+package com.odiga.fiesta;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+public abstract class IntegrationTestSupport {
+}
