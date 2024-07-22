d6b0efae0adf0d3155ca068fa444ab658dfbecae
dar-jeeling <o_o___t@pusan.ac.kr>
2024-07-23 03:55:10 +0900

    feat: `RepositoryTestSupport` 구현

---
+++ src/test/java/com/odiga/fiesta/RepositoryTestSupport.java
@@ -0,0 +1,11 @@
+package com.odiga.fiesta;
+
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@DataJpaTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public abstract class RepositoryTestSupport {
+}
