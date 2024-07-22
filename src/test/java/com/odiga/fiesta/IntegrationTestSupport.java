f3b4be9303684b299dbd20ba706274c6dcb7a937
dar-jeeling <o_o___t@pusan.ac.kr>
2024-07-23 03:56:14 +0900

    feat: `IntegrationTestSupport` 에 `@Transactional` 애노테이션 추가

--- src/test/java/com/odiga/fiesta/IntegrationTestSupport.java
+++ src/test/java/com/odiga/fiesta/IntegrationTestSupport.java
@@ -2,8 +2,10 @@ package com.odiga.fiesta;
 
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
 @ActiveProfiles("test")
+@Transactional
 @SpringBootTest
 public abstract class IntegrationTestSupport {
 }
