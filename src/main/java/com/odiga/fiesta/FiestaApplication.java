2af08f4d993ac574e93763d795a87a593dd15346
dar-jeeling <o_o___t@pusan.ac.kr>
2024-07-23 04:41:56 +0900

    refactor: 기본 application 파일 이름 변경

--- src/main/java/com/odiga/fiesta/FiestaApplication.java
+++ src/main/java/com/odiga/fiesta/FiestaApplication.java
@@ -6,10 +6,10 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
-public class DemoApplication {
+public class FiestaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(FiestaApplication.class, args);
 	}
 
 }
