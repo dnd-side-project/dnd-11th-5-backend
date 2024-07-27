500fd026a33d742a39fd2424aac0d8444f6ac5af
Daeun <74234333+punchdrunkard@users.noreply.github.com>
2024-07-27 15:47:24 +0900

    feat: http request 에 대한 logging 구현 (#16)

---
+++ src/main/java/com/odiga/fiesta/common/filter/LogFilter.java
@@ -0,0 +1,50 @@
+package com.odiga.fiesta.common.filter;
+
+import java.io.IOException;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.constraints.NotNull;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class LogFilter extends OncePerRequestFilter {
+
+	@Override
+	protected void doFilterInternal(
+		final @NonNull HttpServletRequest request,
+		final @NonNull HttpServletResponse response,
+		final @NonNull FilterChain filterChain
+	) throws ServletException, IOException {
+		long requestTime = System.currentTimeMillis();
+
+		filterChain.doFilter(request, response);
+
+		long responseTime = System.currentTimeMillis();
+
+		log(request, response, responseTime - requestTime);
+	}
+
+	private void log(
+		final @NonNull HttpServletRequest request,
+		final @NonNull HttpServletResponse response,
+		final long duration
+	) {
+		String requestURI = request.getRequestURI();
+		String requestMethod = request.getMethod();
+		int responseStatus = response.getStatus();
+
+		log.info("request URI: {}, request method: {}, response status: {}, duration: {} ms",
+			requestURI, requestMethod, responseStatus, duration);
+	}
+}
