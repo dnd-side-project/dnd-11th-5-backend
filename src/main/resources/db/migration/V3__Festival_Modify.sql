5b9261ef544b596b8ebce7abe34ac8f84f154e34
dar-jeeling <o_o___t@pusan.ac.kr>
2024-08-06 20:52:17 +0900

    fix: sql 문법 수정

--- src/main/resources/db/migration/V3__Festival_Modify.sql
+++ src/main/resources/db/migration/V3__Festival_Modify.sql
@@ -1,3 +1,2 @@
-ALTER TABLE `festival`
-    MODIFY `playtime` varchar(255) NULL,
-    MODIFY `fee` varchar(255) NULL;
+ALTER TABLE `festival` MODIFY `playtime` varchar(255) NULL;
+ALTER TABLE `festival` MODIFY `fee` varchar(255) NULL;
