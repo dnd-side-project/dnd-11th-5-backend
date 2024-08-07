973ea0dc52e9d263221577222863d5cf27919a4c
dar-jeeling <o_o___t@pusan.ac.kr>
2024-08-07 21:48:18 +0900

    feat: dto 구현

---
+++ src/main/java/com/odiga/fiesta/festival/dto/response/PriorityResponse.java
@@ -0,0 +1,23 @@
+package com.odiga.fiesta.festival.dto.response;
+
+import static lombok.AccessLevel.*;
+
+import com.odiga.fiesta.festival.domain.Priority;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor(access = PRIVATE)
+public class PriorityResponse {
+
+	private final Long priorityId;
+	private final String priority;
+
+	public static PriorityResponse of(final Priority priority) {
+		return new PriorityResponse(
+			priority.getId(),
+			priority.getPriority()
+		);
+	}
+}
