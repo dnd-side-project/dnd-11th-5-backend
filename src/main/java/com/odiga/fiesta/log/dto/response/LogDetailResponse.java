8b6b52f9736bda473bd3abf65dbc97220cc1df1b
dar-jeeling <o_o___t@pusan.ac.kr>
2024-08-12 02:59:11 +0900

    fix: 날짜 형식 변경

--- src/main/java/com/odiga/fiesta/log/dto/response/LogDetailResponse.java
+++ src/main/java/com/odiga/fiesta/log/dto/response/LogDetailResponse.java
@@ -1,7 +1,11 @@
 package com.odiga.fiesta.log.dto.response;
 
+import static com.fasterxml.jackson.annotation.JsonFormat.Shape.*;
+
 import java.time.LocalDateTime;
 import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.Builder;
 import lombok.Getter;
@@ -12,6 +16,8 @@ public class LogDetailResponse {
 
 	private Long logId;
 	private String title;
+
+	@JsonFormat(shape = STRING, pattern = "yyyy-MM-dd")
 	private LocalDateTime date; // 페스티벌에 다녀온 일자
 	private String address;
 	private Double rating;
