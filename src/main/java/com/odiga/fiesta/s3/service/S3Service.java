808ec97f1d7b0af2b29af5aa47d3fbc655465547
dar-jeeling <o_o___t@pusan.ac.kr>
2024-08-12 21:09:11 +0900

    feat: 이미지 삭제, validation 구현

--- src/main/java/com/odiga/fiesta/s3/service/S3Service.java
+++ src/main/java/com/odiga/fiesta/s3/service/S3Service.java
@@ -1,9 +1,8 @@
 package com.odiga.fiesta.s3.service;
 
-import static com.odiga.fiesta.common.error.ErrorCode.*;
-
 import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
@@ -13,7 +12,9 @@ import org.springframework.web.multipart.MultipartFile;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.PutObjectRequest;
-import com.odiga.fiesta.common.error.exception.CustomException;
+import com.odiga.fiesta.common.error.exception.s3.InvalidExtensionException;
+import com.odiga.fiesta.common.error.exception.s3.InvalidImageException;
+import com.odiga.fiesta.common.error.exception.s3.UploadFailException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -30,41 +31,50 @@ public class S3Service {
 	@Value("${cloud.aws.s3.bucket}")
 	private String bucket;
 
-	private String uploadImage(final MultipartFile file) throws IOException {
-		final String fileName = getFileName(file);
+	public String uploadImage(final MultipartFile file, final String dirName) throws IOException {
+		if (file.isEmpty()) {
+			throw new UploadFailException();
+		}
+
+		final String fileName = file.getOriginalFilename();
+
 		final String extension = getFileExtension(fileName);
 		validateImageExtension(extension);
 
-		return upload(file);
+		return upload(file, dirName);
 	}
 
-	private String upload(final MultipartFile file) throws IOException {
-		String fileName = getFileName(file);
+	public String upload(final MultipartFile file, final String dirName) throws IOException {
+		String fileName = dirName + "/" + UUID.randomUUID() + file.getOriginalFilename();
 		return putS3(file, fileName);
 	}
 
-	private String putS3(final MultipartFile file, String fileName) throws IOException {
+	public String putS3(final MultipartFile file, String fileName) throws IOException {
 		amazonS3Client.putObject(new PutObjectRequest(bucket, fileName, file.getInputStream(), null)
 			.withCannedAcl(CannedAccessControlList.PublicRead));
 		return String.valueOf(amazonS3Client.getUrl(bucket, fileName));
 	}
 
-	private String getFileName(final MultipartFile file) {
-		return file.getOriginalFilename();
+	public void removeFile(String path, String dirName) {
+		amazonS3Client.deleteObject(bucket, getKey(path, dirName));
 	}
 
-	private String getFileExtension(final String fileName) {
+	private String getKey(String path, String dirName) {
+		return path.substring(path.indexOf(dirName));
+	}
+
+	public String getFileExtension(final String fileName) {
 		final int index = fileName.lastIndexOf(".");
 		if (index > 0 && fileName.length() > index + 1) {
 			return fileName.substring(index + 1);
 		} else {
-			throw new CustomException(INVALID_EXTENSION_TYPE);
+			throw new InvalidExtensionException();
 		}
 	}
 
-	private void validateImageExtension(final String extension) {
+	public void validateImageExtension(final String extension) {
 		if (!PERMISSION_IMG_EXTENSIONS.contains(extension)) {
-			throw new CustomException(INVALID_IMAGE_TYPE);
+			throw new InvalidImageException();
 		}
 	}
 }
