98e5906b823295e7ce274db390e21aea019f6fbd
dar-jeeling <o_o___t@pusan.ac.kr>
2024-08-13 16:17:56 +0900

    feat: 페스티벌 날짜 별 그룹핑 로직 작성

--- src/main/java/com/odiga/fiesta/festival/domain/Festival.java
+++ src/main/java/com/odiga/fiesta/festival/domain/Festival.java
@@ -3,9 +3,15 @@ package com.odiga.fiesta.festival.domain;
 import static jakarta.persistence.GenerationType.*;
 import static lombok.AccessLevel.*;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.AbstractMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import com.odiga.fiesta.common.domain.BaseEntity;
+import com.odiga.fiesta.festival.dto.response.FestivalSimpleResponse;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -23,58 +29,67 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = PROTECTED)
 public class Festival extends BaseEntity {
 
-    @Id
-    @GeneratedValue(strategy = IDENTITY)
-    @Column(name = "festival_id")
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = IDENTITY)
+	@Column(name = "festival_id")
+	private Long id;
 
-    @Column(name = "user_id")
-    private Long userId;
+	@Column(name = "user_id")
+	private Long userId;
 
-    @Column(nullable = false)
-    private String name;
+	@Column(nullable = false)
+	private String name;
 
-    @Column(name = "start_date", nullable = false)
-    private LocalDateTime startDate;
+	@Column(name = "start_date", nullable = false)
+	private LocalDateTime startDate;
 
-    @Column(name = "end_date", nullable = false)
-    private LocalDateTime endDate;
+	@Column(name = "end_date", nullable = false)
+	private LocalDateTime endDate;
 
-    @Column(nullable = false)
-    private String address;
+	@Column(nullable = false)
+	private String address;
 
-    @Column(name = "sido_id", nullable = false)
-    private Long sidoId;
+	@Column(name = "sido_id", nullable = false)
+	private Long sidoId;
 
-    @Column(nullable = false)
-    private String sigungu;
+	@Column(nullable = false)
+	private String sigungu;
 
-    @Column(nullable = false)
-    private double latitude;
+	@Column(nullable = false)
+	private double latitude;
 
-    @Column(nullable = false)
-    private double longitude;
+	@Column(nullable = false)
+	private double longitude;
 
-    @Column(nullable = false)
-    private String tip;
+	@Column(nullable = false)
+	private String tip;
 
-    @Column(name = "homepage_url", length = 1024)
-    private String homepageUrl;
+	@Column(name = "homepage_url", length = 1024)
+	private String homepageUrl;
 
-    @Column(name = "instagram_url", length = 1024)
-    private String instagramUrl;
+	@Column(name = "instagram_url", length = 1024)
+	private String instagramUrl;
 
-    private String fee;
+	private String fee;
 
-    @Column(nullable = false)
-    private String description;
+	@Column(nullable = false)
+	private String description;
 
-    @Column(name = "ticket_link", length = 1024)
-    private String ticketLink;
+	@Column(name = "ticket_link", length = 1024)
+	private String ticketLink;
 
-    private String playtime;
+	private String playtime;
 
-    @Column(name = "is_pending", nullable = false)
-    private boolean isPending;
+	@Column(name = "is_pending", nullable = false)
+	private boolean isPending;
 
+	public static Map<LocalDate, List<Festival>> getGroupedByDate(List<Festival> festivals) {
+		return festivals.stream()
+			.flatMap(festival ->
+				festival.getStartDate().toLocalDate().datesUntil(festival.getEndDate().toLocalDate().plusDays(1))
+					.map(date -> new AbstractMap.SimpleEntry<>(date, festival)))
+			.collect(
+				Collectors.groupingBy(entry -> entry.getKey(),
+					Collectors.mapping(entry -> entry.getValue(), Collectors.toList())));
+	}
 }
