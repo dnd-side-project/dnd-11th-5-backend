bd6d1e73c237dcf2a6e548a1ad62d58d97a3c814
dar-jeeling <o_o___t@pusan.ac.kr>
2024-08-13 14:33:49 +0900

    refactor: import 문 정리

--- src/main/java/com/odiga/fiesta/festival/repository/FestivalRepository.java
+++ src/main/java/com/odiga/fiesta/festival/repository/FestivalRepository.java
@@ -1,7 +1,8 @@
 package com.odiga.fiesta.festival.repository;
 
-import com.odiga.fiesta.festival.domain.Festival;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.odiga.fiesta.festival.domain.Festival;
 
 public interface FestivalRepository extends JpaRepository<Festival, Long> {
 }
