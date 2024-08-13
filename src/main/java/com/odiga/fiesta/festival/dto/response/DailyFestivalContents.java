a24ca927e47f7fff334cdd768e7de148e0c7e790
dar-jeeling <o_o___t@pusan.ac.kr>
2024-08-13 23:45:37 +0900

    feat: 페스티벌 월별 조회 API 구현

---
+++ src/main/java/com/odiga/fiesta/festival/dto/response/DailyFestivalContents.java
@@ -0,0 +1,23 @@
+package com.odiga.fiesta.festival.dto.response;
+
+import static com.fasterxml.jackson.annotation.JsonFormat.Shape.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@Builder
+@ToString
+public class DailyFestivalContents {
+
+	@JsonFormat(shape = STRING, pattern = "yyyy-MM-dd")
+	private LocalDate date;
+	private List<FestivalSimpleResponse> festivals;
+	private Integer totalElements;
+}
--- src/test/java/com/odiga/fiesta/festival/controller/FestivalControllerTest.java
+++ src/test/java/com/odiga/fiesta/festival/controller/FestivalControllerTest.java
@@ -1,111 +1,50 @@
 package com.odiga.fiesta.festival.controller;
 
-import static org.mockito.Mockito.*;
+import static org.mockito.BDDMockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
-import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 
 import com.odiga.fiesta.ControllerTestSupport;
-import com.odiga.fiesta.festival.dto.response.CategoryResponse;
-import com.odiga.fiesta.festival.dto.response.CompanionResponse;
-import com.odiga.fiesta.festival.dto.response.MoodResponse;
-import com.odiga.fiesta.festival.dto.response.PriorityResponse;
-import com.odiga.fiesta.festival.service.CategoryService;
-import com.odiga.fiesta.festival.service.CompanionService;
-import com.odiga.fiesta.festival.service.MoodService;
-import com.odiga.fiesta.festival.service.PriorityService;
+import com.odiga.fiesta.festival.dto.response.FestivalMonthlyResponse;
+import com.odiga.fiesta.festival.service.FestivalService;
 
 class FestivalControllerTest extends ControllerTestSupport {
 
-	@Autowired
-	private MoodService moodService;
+	private final String PREFIX = "/api/v1/festivals";
 
 	@Autowired
-	private CategoryService categoryService;
+	private FestivalService festivalService;
 
-	@Autowired
-	private CompanionService companionService;
-
-	@Autowired
-	private PriorityService priorityService;
-
-	@DisplayName("모든 페스티벌 분위기를 조회한다.")
+	@DisplayName("유효하지 않은 값의 month 값이 들어오면 에러가 발생한다.")
 	@Test
-	void getAllMoods() throws Exception {
-		// given
-		String message = "페스티벌 분위기 조회 성공";
-
-		List<MoodResponse> mockMoods = List.of();
-		when(moodService.getAllMoods()).thenReturn(mockMoods);
-
-		// when // then
-		mockMvc.perform(
-				get("/api/v1/festivals/moods")
-			).andDo(print())
-			.andExpect(status().isOk())
-			.andExpect(jsonPath("$.message").value(message))
-			.andExpect(jsonPath("$.data").isArray());
+	void getMonthlyFestivals_invalidParameter() throws Exception {
+		// given // when // then
+		mockMvc.perform(get(PREFIX + "/monthly")
+				.param("year", "2023")
+				.param("month", "13"))
+			.andExpect(status().isBadRequest())
+			.andExpect(jsonPath("$.code").value("C003"));
 	}
 
-	@DisplayName("모든 카테고리를 조회한다.")
+	@DisplayName("특정 년, 월의 페스티벌들을 조회한다.")
 	@Test
-	void getAllCategories() throws Exception {
-		// given
-		String message = "페스티벌 카테고리 조회 성공";
+	void getMonthlyFestivals() throws Exception {
+		// Given
+		FestivalMonthlyResponse mockResponse = FestivalMonthlyResponse.builder().build();
+		when(festivalService.getMonthlyFestivals(2024, 10)).thenReturn(mockResponse);
 
-		List<CategoryResponse> mockCategories = List.of();
-		when(categoryService.getAllCategories()).thenReturn(mockCategories);
-
-		// when // then
-		mockMvc.perform(
-				get("/api/v1/festivals/categories")
-			).andDo(print())
+		// When & Then
+		mockMvc.perform(get(PREFIX + "/monthly")
+				.param("year", "2024")
+				.param("month", "10")
+				.contentType(MediaType.APPLICATION_JSON))
 			.andExpect(status().isOk())
-			.andExpect(jsonPath("$.message").value(message))
-			.andExpect(jsonPath("$.data").isArray());
-
+			.andExpect(jsonPath("$.message").value("페스티벌 월간 조회 성공"))
+			.andExpect(jsonPath("$.data").isNotEmpty());
 	}
-
-	@DisplayName("모든 일행 타입을 조회한다.")
-	@Test
-	void getAllCompanions() throws Exception {
-		// given
-		String message = "페스티벌 일행 분류 조회 성공";
-
-		List<CompanionResponse> mockCompanions = List.of();
-		when(companionService.getAllCompanions()).thenReturn(mockCompanions);
-
-		// when // then
-		mockMvc.perform(
-				get("/api/v1/festivals/companions")
-			).andDo(print())
-			.andExpect(status().isOk())
-			.andExpect(jsonPath("$.message").value(message))
-			.andExpect(jsonPath("$.data").isArray());
-	}
-
-	@DisplayName("모든 페스티벌 우선순위 타입을 조회한다.")
-	@Test
-	void getAllPriorities() throws Exception {
-		// given
-		String message = "페스티벌 우선순위 조회 성공";
-
-		List<PriorityResponse> mockPriorities = List.of();
-		when(priorityService.getAllPriorities()).thenReturn(mockPriorities);
-
-		// when // then
-		mockMvc.perform(
-				get("/api/v1/festivals/priorities")
-			).andDo(print())
-			.andExpect(status().isOk())
-			.andExpect(jsonPath("$.message").value(message))
-			.andExpect(jsonPath("$.data").isArray());
-	}
-
 }
