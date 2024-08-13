16082dc5d7d3eeae1899895127dee7f081c22259
dar-jeeling <o_o___t@pusan.ac.kr>
2024-08-14 05:53:59 +0900

    feat: 관련 엔티티 생성

---
+++ src/main/java/com/odiga/fiesta/festival/domain/FestivalImage.java
@@ -0,0 +1,34 @@
+package com.odiga.fiesta.festival.domain;
+
+import static jakarta.persistence.GenerationType.*;
+import static lombok.AccessLevel.*;
+
+import com.odiga.fiesta.common.domain.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor(access = PRIVATE)
+@NoArgsConstructor
+public class FestivalImage extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = IDENTITY)
+	@Column(name = "festival_image_id")
+	private Long id;
+
+	@Column(name = "festival_id", nullable = false)
+	private Long festivalId;
+
+	@Column(name = "image_url", length = 2083, nullable = false)
+	private String imageUrl;
+}
