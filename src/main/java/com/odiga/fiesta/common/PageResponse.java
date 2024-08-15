be5c11afc98ca97c1e49310f84294a031456b2ea
dar-jeeling <o_o___t@pusan.ac.kr>
2024-08-15 18:17:09 +0900

    refactor: `PageResponse`에 `of` 작성

--- src/main/java/com/odiga/fiesta/common/PageResponse.java
+++ src/main/java/com/odiga/fiesta/common/PageResponse.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.domain.Page;
 
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
@@ -15,12 +16,17 @@ public class PageResponse<T> {
 	private final long totalElements;
 	private final int totalPages;
 
+	@Builder
 	public PageResponse(Page<T> page) {
 		this.content = page.getContent();
 		this.offset = page.getPageable().getOffset();
 		this.pageSize = page.getPageable().getPageSize();
 		this.totalElements = page.getTotalElements();
 		this.totalPages = page.getTotalPages();
+	}
+
+	public static <T> PageResponse<T> of(Page<T> page) {
+		return new PageResponse<>(page);
 	}
 }
 
