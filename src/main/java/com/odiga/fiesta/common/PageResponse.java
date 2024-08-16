6202b4a0a67c08cd30d4f9537caafbe3ccb1ff69
dar-jeeling <o_o___t@pusan.ac.kr>
2024-08-16 23:42:10 +0900

    fix: 누락된 필드 추가

--- src/main/java/com/odiga/fiesta/common/PageResponse.java
+++ src/main/java/com/odiga/fiesta/common/PageResponse.java
@@ -13,6 +13,7 @@ public class PageResponse<T> {
 	private final List<T> content;
 	private final long offset;
 	private final int pageSize;
+	private final int pageNumber;
 	private final long totalElements;
 	private final int totalPages;
 
@@ -21,8 +22,20 @@ public class PageResponse<T> {
 		this.content = page.getContent();
 		this.offset = page.getPageable().getOffset();
 		this.pageSize = page.getPageable().getPageSize();
+		this.pageNumber = page.getPageable().getPageNumber();
 		this.totalElements = page.getTotalElements();
 		this.totalPages = page.getTotalPages();
+	}
+
+	@Builder
+	public PageResponse(List<T> content, long offset, int pageSize, int pageNumber, long totalElements,
+		int totalPages) {
+		this.content = content;
+		this.offset = offset;
+		this.pageSize = pageSize;
+		this.pageNumber = pageNumber;
+		this.totalElements = totalElements;
+		this.totalPages = totalPages;
 	}
 
 	public static <T> PageResponse<T> of(Page<T> page) {
