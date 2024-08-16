bacc79f82b445f2cdd7bd2f22bc2d2881d4fd97c
dar-jeeling <o_o___t@pusan.ac.kr>
2024-08-16 23:42:32 +0900

    env: 캐싱, 비동기 활성화

--- src/main/java/com/odiga/fiesta/FiestaApplication.java
+++ src/main/java/com/odiga/fiesta/FiestaApplication.java
@@ -2,10 +2,14 @@ package com.odiga.fiesta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
+@EnableCaching
+@EnableAsync
 public class FiestaApplication {
 
 	public static void main(String[] args) {
