4aec8a78b78d0cfe4da3305ae9c60b97149797a5
dar-jeeling <o_o___t@pusan.ac.kr>
2024-08-16 23:41:47 +0900

    feat: 검색 관련 redis 설정

--- src/main/java/com/odiga/fiesta/common/config/RedisConfig.java
+++ src/main/java/com/odiga/fiesta/common/config/RedisConfig.java
@@ -3,9 +3,13 @@ package com.odiga.fiesta.common.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import lombok.RequiredArgsConstructor;
@@ -26,8 +30,8 @@ public class RedisConfig {
 	}
 
 	@Bean
-	public RedisTemplate<String, Object> redisTemplate() {
-		RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+	public <T> RedisTemplate<String, T> redisTemplate() {
+		RedisTemplate<String, T> redisTemplate = new RedisTemplate<>();
 		redisTemplate.setConnectionFactory(redisConnectionFactory());
 
 		// 일반적인 key:value의 경우 시리얼라이저
@@ -42,5 +46,20 @@ public class RedisConfig {
 		redisTemplate.setDefaultSerializer(new StringRedisSerializer());
 
 		return redisTemplate;
+	}
+
+	@Bean
+	public RedisCacheManager redisCacheManager(RedisConnectionFactory redisConnectionFactory) {
+		RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+			.serializeKeysWith(RedisSerializationContext
+				.SerializationPair.fromSerializer(new StringRedisSerializer()))
+			.serializeValuesWith(RedisSerializationContext
+				.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+
+		return RedisCacheManager
+			.RedisCacheManagerBuilder
+			.fromConnectionFactory(redisConnectionFactory)
+			.cacheDefaults(redisCacheConfiguration)
+			.build();
 	}
 }
--- src/main/java/com/odiga/fiesta/common/util/RedisUtils.java
+++ src/main/java/com/odiga/fiesta/common/util/RedisUtils.java
@@ -2,7 +2,10 @@ package com.odiga.fiesta.common.util;
 
 import static java.util.concurrent.TimeUnit.*;
 
+import java.util.Set;
+
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
@@ -11,19 +14,38 @@ import lombok.RequiredArgsConstructor;
 
 @Component
 @RequiredArgsConstructor
-public class RedisUtils {
+public class RedisUtils<T> {
 
-	private final RedisTemplate<String, Object> redisTemplate;
+	private final RedisTemplate<String, T> redisTemplate;
 
-	public void setData(String key, String value, Long expiredTime) {
+	public void setData(String key, T value, Long expiredTime) {
 		redisTemplate.opsForValue().set(key, value, expiredTime, MILLISECONDS);
 	}
 
-	public String getData(String key) {
-		return (String)redisTemplate.opsForValue().get(key);
+	public T getData(String key, Class<T> clazz) {
+		Object value = redisTemplate.opsForValue().get(key);
+		return clazz.cast(value);
 	}
 
 	public void deleteData(String key) {
 		redisTemplate.delete(key);
 	}
+
+	// 실시간 검색어 관련
+	public Double zScore(String key, T member) {
+		return redisTemplate.opsForZSet().score(key, member);
+	}
+
+	public void zAdd(String key, T target, Double score) {
+		redisTemplate.opsForZSet().add(key, target, score);
+	}
+
+	public Set<ZSetOperations.TypedTuple<T>> zRevrange(String key, long start, long end) {
+		return redisTemplate.opsForZSet().reverseRangeWithScores(key, start, end);
+	}
+
+	public Long zSize(String key){
+		return redisTemplate.opsForZSet().size(key);
+	}
+
 }
