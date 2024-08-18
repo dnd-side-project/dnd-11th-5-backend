227510b0632a5f42534f24c6e4c8441c5092f2c9
dar-jeeling <o_o___t@pusan.ac.kr>
2024-08-18 14:53:46 +0900

    hotfix: Query did not return a unique result 해결

--- src/main/java/com/odiga/fiesta/festival/repository/FestivalImageRepository.java
+++ src/main/java/com/odiga/fiesta/festival/repository/FestivalImageRepository.java
@@ -1,6 +1,5 @@
 package com.odiga.fiesta.festival.repository;
 
-
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,7 +10,7 @@ import com.odiga.fiesta.festival.domain.FestivalImage;
 
 public interface FestivalImageRepository extends JpaRepository<FestivalImage, Long> {
 
-	@Query("SELECT fi.imageUrl FROM FestivalImage fi WHERE fi.festivalId = :festivalId")
+	@Query(value = "SELECT fi.image_url FROM festival_image fi WHERE fi.festival_id = :festivalId LIMIT 1", nativeQuery = true)
 	String findFirstImageUrlByFestivalId(@Param("festivalId") Long festivalId);
 
 	List<FestivalImage> findAllByFestivalId(Long festivalId);
