048b036f4e8f6377d00d1fe69b69ea6c57924552
dar-jeeling <o_o___t@pusan.ac.kr>
2024-08-18 14:23:50 +0900

    fix: `@NoArgsConstructor` 추가

--- src/main/java/com/odiga/fiesta/log/domain/LogImage.java
+++ src/main/java/com/odiga/fiesta/log/domain/LogImage.java
@@ -1,6 +1,7 @@
 package com.odiga.fiesta.log.domain;
 
 import static jakarta.persistence.GenerationType.*;
+import static lombok.AccessLevel.*;
 
 import com.odiga.fiesta.common.domain.BaseEntity;
 
@@ -10,11 +11,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "log_image")
 @Getter
+@NoArgsConstructor(access = PROTECTED)
 public class LogImage extends BaseEntity {
 
 	@Id
@@ -29,4 +33,11 @@ public class LogImage extends BaseEntity {
 	@Column(name = "image_url", length = 2083)
 	@NotNull
 	private String imageUrl;
+
+	@Builder
+	public LogImage(Long id, Long logId, String imageUrl) {
+		this.id = id;
+		this.logId = logId;
+		this.imageUrl = imageUrl;
+	}
 }
