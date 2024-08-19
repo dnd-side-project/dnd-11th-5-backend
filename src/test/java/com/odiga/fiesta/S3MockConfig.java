be66599439025853e045a0b8f65abe2a67ee9f87
dar-jeeling <o_o___t@pusan.ac.kr>
2024-08-19 19:17:26 +0900

    feat: `S3MockConfig` 작성

--- src/test/java/com/odiga/fiesta/S3MockConfig.java
+++ src/test/java/com/odiga/fiesta/S3MockConfig.java
@@ -1,20 +1,48 @@
 package com.odiga.fiesta;
 
-import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.AnonymousAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.s3.AmazonS3Client;
-import com.odiga.fiesta.common.config.S3Config;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+import akka.http.scaladsl.Http;
+import io.findify.s3mock.S3Mock;
 
 @TestConfiguration
-public class S3MockConfig extends S3Config {
+public class S3MockConfig {
+	@Value("${cloud.aws.region.static}")
+	private String region;
+
+	@Value("${cloud.aws.s3.bucket}")
+	private String bucket;
 
 	@Bean
+	public S3Mock s3Mock() {
+		return new S3Mock.Builder().withPort(0).withInMemoryBackend().build();
+	}
+
+	@Bean(destroyMethod = "shutdown")
 	@Primary
-	@Override
-	public AmazonS3Client amazonS3Client() {
-		return Mockito.mock(AmazonS3Client.class);
+	public AmazonS3Client amazonS3Client(S3Mock s3Mock) {
+		Http.ServerBinding serverBinding = s3Mock.start();
+		int port = serverBinding.localAddress().getPort();
+
+		AwsClientBuilder.EndpointConfiguration endpoint = new AwsClientBuilder.EndpointConfiguration(
+			"http://localhost:" + port, region);
+		AmazonS3Client client = (AmazonS3Client)AmazonS3ClientBuilder
+			.standard()
+			.withPathStyleAccessEnabled(true)
+			.withEndpointConfiguration(endpoint)
+			.withCredentials(new AWSStaticCredentialsProvider(
+				new AnonymousAWSCredentials()
+			)).build();
+		client.createBucket(bucket);
+		return client;
 	}
 }
