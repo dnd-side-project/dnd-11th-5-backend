d2ef29c7ded069dad3b71df2f66b995bde1f7730
dar-jeeling <o_o___t@pusan.ac.kr>
2024-08-21 23:34:44 +0900

    fix: 배포환경의 swagger에서의 cors 문제 해결

--- src/main/java/com/odiga/fiesta/common/config/SecurityConfig.java
+++ src/main/java/com/odiga/fiesta/common/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.odiga.fiesta.common.config;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
@@ -27,63 +28,67 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final TokenProvider tokenProvider;
+	private final TokenProvider tokenProvider;
 
-    /**
-     * 스프링 시큐리티 기능 비활성화
-     */
-    @Bean
-    public WebSecurityCustomizer configure() {
-        return web -> web.ignoring()
-                // 정적 리소스에 대한 스프링 시큐리티 사용을 비활성화
-                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
-    }
+	/**
+	 * 스프링 시큐리티 기능 비활성화
+	 */
+	@Bean
+	public WebSecurityCustomizer configure() {
+		return web -> web.ignoring()
+			// 정적 리소스에 대한 스프링 시큐리티 사용을 비활성화
+			.requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+	}
 
-    /**
-     * 특정 HTTP 요청에 대한 웹 기반 보안 구성
-     */
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(AbstractHttpConfigurer::disable) //csrf disable
-                .formLogin(AbstractHttpConfigurer::disable) //From 로그인 방식 disable
-                .httpBasic(AbstractHttpConfigurer::disable) //HTTP Basic 인증 방식 disable
+	/**
+	 * 특정 HTTP 요청에 대한 웹 기반 보안 구성
+	 */
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+			.csrf(AbstractHttpConfigurer::disable) //csrf disable
+			.formLogin(AbstractHttpConfigurer::disable) //From 로그인 방식 disable
+			.httpBasic(AbstractHttpConfigurer::disable) //HTTP Basic 인증 방식 disable
 
-                // 토큰 기반 인증을 사용하기 때문에 세션 기능 비활성화
-                .sessionManagement((session) -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+			// 토큰 기반 인증을 사용하기 때문에 세션 기능 비활성화
+			.sessionManagement((session) -> session
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 
-                // 인증, 인가 설정
-                .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/**").permitAll()
-                        .anyRequest().authenticated())
+			// 인증, 인가 설정
+			.authorizeHttpRequests((auth) -> auth
+				.requestMatchers("/**").permitAll()
+				.anyRequest().authenticated())
 
-                // cors 설정
-                .cors(corsCustomizer -> corsCustomizer.configurationSource(request -> {
-                    CorsConfiguration configuration = new CorsConfiguration();
-                    configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
-                    configuration.setAllowedMethods(Collections.singletonList("*"));
-                    configuration.setAllowCredentials(true);
-                    configuration.setAllowedHeaders(Collections.singletonList("*"));
-                    configuration.setMaxAge(3600L);
-                    configuration.setExposedHeaders(Arrays.asList("Set-Cookie", "Authorization"));
-                    return configuration;
-                }))
+			// cors 설정
+			.cors(corsCustomizer -> corsCustomizer.configurationSource(request -> {
+				CorsConfiguration configuration = new CorsConfiguration();
+				configuration.setAllowedOrigins(Arrays.asList(
+					"http://localhost:3000",
+					"https://odiga.shop",
+					"https://fiesta-psi.vercel.app"
+				));
+				configuration.setAllowedMethods(Collections.singletonList("*"));
+				configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+				configuration.setExposedHeaders(Arrays.asList("Set-Cookie", "Authorization"));
+				configuration.setAllowCredentials(true);
+				configuration.setMaxAge(3600L);
+				return configuration;
+			}))
 
-                // 헤더를 확인할 커스텀 필터 추가
-                .addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+			// 헤더를 확인할 커스텀 필터 추가
+			.addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
 
-                // 인증 및 인가 예외 처리
-                .exceptionHandling(exceptions -> exceptions
-                        .authenticationEntryPoint(new JwtAuthenticationEntryPoint())
-                        .accessDeniedHandler(new JwtAccessDeniedHandler())
-                );
+			// 인증 및 인가 예외 처리
+			.exceptionHandling(exceptions -> exceptions
+				.authenticationEntryPoint(new JwtAuthenticationEntryPoint())
+				.accessDeniedHandler(new JwtAccessDeniedHandler())
+			);
 
-        return http.build();
-    }
+		return http.build();
+	}
 
-    @Bean
-    public TokenAuthenticationFilter tokenAuthenticationFilter() {
-        return new TokenAuthenticationFilter(tokenProvider);
-    }
+	@Bean
+	public TokenAuthenticationFilter tokenAuthenticationFilter() {
+		return new TokenAuthenticationFilter(tokenProvider);
+	}
 }
