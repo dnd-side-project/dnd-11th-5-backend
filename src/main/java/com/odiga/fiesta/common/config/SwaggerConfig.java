c2e749b24607850c218cfefc24530e9684ec7586
dar-jeeling <o_o___t@pusan.ac.kr>
2024-08-21 16:41:27 +0900

    env: swagger 에서 인증 사용 가능 설정 추가

--- src/main/java/com/odiga/fiesta/common/config/SwaggerConfig.java
+++ src/main/java/com/odiga/fiesta/common/config/SwaggerConfig.java
@@ -5,17 +5,27 @@ import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class SwaggerConfig {
 
-    @Bean
-    public OpenAPI openAPI() {
-        Info info = new Info()
-                .version("v1.0") //버전
-                .title("Fiesta API") //이름
-                .description("Fiesta API"); //설명
-        return new OpenAPI()
-                .info(info);
-    }
+	@Bean
+	public OpenAPI openAPI() {
+		return new OpenAPI()
+			.info(new Info()
+				.version("v1.0") // 버전
+				.title("Fiesta API") // 이름
+				.description("Fiesta API")) // 설명
+			.addSecurityItem(new SecurityRequirement()
+				.addList("BearerAuth"))
+			.components(new io.swagger.v3.oas.models.Components()
+				.addSecuritySchemes("BearerAuth", new SecurityScheme()
+					.type(SecurityScheme.Type.HTTP)
+					.scheme("bearer")
+					.bearerFormat("JWT")
+					.in(SecurityScheme.In.HEADER)
+					.name("Authorization")));
+	}
 }
