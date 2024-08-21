eb4774925ab7c1ebca3b2786f6113faee5130084
yueunfive <122276414+yueunfive@users.noreply.github.com>
2024-08-21 17:28:21 +0900

    refactor: 리뷰 테스트 제외 및 스웨거 인증 추가

--- src/main/java/com/odiga/fiesta/common/config/SwaggerConfig.java
+++ src/main/java/com/odiga/fiesta/common/config/SwaggerConfig.java
@@ -1,5 +1,8 @@
 package com.odiga.fiesta.common.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,11 +14,24 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
+        // Bearer Token을 위한 SecurityScheme 정의
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT");
+
+        // SecurityRequirement 정의
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList("bearerAuth");
+
+
         Info info = new Info()
                 .version("v1.0") //버전
                 .title("Fiesta API") //이름
                 .description("Fiesta API"); //설명
         return new OpenAPI()
-                .info(info);
+                .info(info)
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+                .addSecurityItem(securityRequirement);
     }
 }
