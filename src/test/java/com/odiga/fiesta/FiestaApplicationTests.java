4a508cdbaf142577ce62a21f228fe6d887055d10
dar-jeeling <o_o___t@pusan.ac.kr>
2024-08-21 16:39:28 +0900

    refactor: 테스트 패키지 폴더 구조 정리

--- src/test/java/com/odiga/fiesta/FiestaApplicationTests.java
+++ src/test/java/com/odiga/fiesta/FiestaApplicationTests.java
@@ -5,6 +5,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
+import com.odiga.fiesta.config.S3MockConfig;
+
 @ActiveProfiles("test")
 @SpringBootTest
 @Import(S3MockConfig.class)
--- src/test/java/com/odiga/fiesta/config/QuerydslTestConfig.java
+++ src/test/java/com/odiga/fiesta/config/QuerydslTestConfig.java
@@ -1,4 +1,4 @@
-package com.odiga.fiesta;
+package com.odiga.fiesta.config;
 
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
