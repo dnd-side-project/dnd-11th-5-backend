24dfa82b961753be03028440a3b06f92c2badade
dar-jeeling <o_o___t@pusan.ac.kr>
2024-08-26 01:00:16 +0900

    hotfix: column 제약 사항 수정

---
+++ src/main/resources/db/migration/V19__festival_allow_null.sql
@@ -0,0 +1,20 @@
+alter table festival
+    add column content_id varchar(255) NULL;
+
+alter table festival
+    modify column fee varchar(1000) NULL;
+
+alter table festival
+    modify column playtime varchar(1000) NULL;
+
+alter table festival
+    modify column homepage_url varchar(2083) NULL;
+
+alter table festival
+    modify column instagram_url varchar(2083) NULL;
+
+alter table festival
+    modify column ticket_link varchar(2083) NULL;
+
+alter table festival
+    modify column tip varchar(1000) NULL;
