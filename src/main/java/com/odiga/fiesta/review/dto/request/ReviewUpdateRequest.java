c1b18d4e46bdbd3faeaeb85bc80edfaafef7d2b3
dar-jeeling <o_o___t@pusan.ac.kr>
2024-08-30 00:59:14 +0900

    feat: list 형태로 들어오는 값들의 중복 제거 로직 추가

--- src/main/java/com/odiga/fiesta/review/dto/request/ReviewUpdateRequest.java
+++ src/main/java/com/odiga/fiesta/review/dto/request/ReviewUpdateRequest.java
@@ -1,5 +1,6 @@
 package com.odiga.fiesta.review.dto.request;
 
+import java.util.Collections;
 import java.util.List;
 
 import jakarta.validation.constraints.AssertTrue;
@@ -9,7 +10,6 @@ import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-@Builder
 public class ReviewUpdateRequest {
 
 	@NotNull
@@ -27,5 +27,17 @@ public class ReviewUpdateRequest {
 	@AssertTrue(message = "평점은 0.5 단위로 입력해야 합니다.")
 	private boolean isRatingValid() {
 		return rating != null && (rating * 10) % 5 == 0;
+	}
+
+	@Builder
+	public ReviewUpdateRequest(Double rating, List<Long> keywordIds, List<Long> deletedImages, String content) {
+		this.rating = rating;
+		this.keywordIds = removeDuplicates(keywordIds);
+		this.deletedImages = removeDuplicates(deletedImages);
+		this.content = content;
+	}
+
+	private List<Long> removeDuplicates(List<Long> list) {
+		return list == null ? Collections.emptyList() : list.stream().distinct().toList();
 	}
 }
