6b2a5c9a779b0ca2cb1034b1f243e64012e055c2
dar-jeeling <o_o___t@pusan.ac.kr>
2024-09-25 06:19:22 +0900

    refactor: id 상수 정의

---
+++ src/main/java/com/odiga/fiesta/category/domain/CategoryConstants.java
@@ -0,0 +1,21 @@
+package com.odiga.fiesta.category.domain;
+
+public class CategoryConstants {
+
+	public static final Long CATEGORY_CULTURE = 1L;
+	public static final Long CATEGORY_MUSIC_AND_DANCE = 2L;
+	public static final Long CATEGORY_MOVIE = 3L;
+	public static final Long CATEGORY_FOOD_AND_DRINK = 4L;
+	public static final Long CATEGORY_ACTIVITY = 5L;
+	public static final Long CATEGORY_ART = 6L;
+	public static final Long CATEGORY_HISTORY = 7L;
+	public static final Long CATEGORY_NATURE = 8L;
+	public static final Long CATEGORY_PETS = 9L;
+	public static final Long CATEGORY_NIGHT = 10L;
+	public static final Long CATEGORY_FIREWORKS = 11L;
+	public static final Long CATEGORY_UNIQUE_FESTIVAL = 12L;
+
+	// 생성자를 private으로 하여 상수 클래스가 인스턴스화되지 않도록 방지
+	private CategoryConstants() {
+	}
+}
